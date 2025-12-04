02e5d3bdcbb70b16e46c1cda9a29af2d9748b195
jagtapatharv2006-design <jagtapatharv2006@gmail.com>
2025-12-04 19:11:17 +0530

    commit message

---
+++ assignment1exe1.py
@@ -0,0 +1,8 @@
+# first take input
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+print("\n")
+print("addition:",num1+num2)
+print("substraction:",num1-num2)
+print("multiplicatoin:",num1*num2)
+print("division",num1/num2)
